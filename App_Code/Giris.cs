af03aa75e85c4e8a2c6e4f07000d3afdfa87dda3
HilalBudak <hilalbudak.97@gmail.com>
2018-07-24 10:19:28 +0300

    App Code

---
+++ App_Code/Giris.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Giris için özet açıklama
+/// </summary>
+public class Giris
+{
+    public class MyClass
+    {
+        static public int Id { get; set; }
+        static public string ad { get; set; }
+    }
+}
