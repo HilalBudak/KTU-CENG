003e8370a9a44ba11c9ee729c1037e344f1bffd2
HilalBudak <hilalbudak.97@gmail.com>
2018-07-24 10:06:08 +0300

    Login.cs

---
+++ Login1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Login1 için özet açıklama
+/// </summary>
+public class Login1
+{
+    public Login1()
+    {
+        //
+        //TODO: Buraya oluşturucu mantığı ekleyin
+        //
+    }
+}
